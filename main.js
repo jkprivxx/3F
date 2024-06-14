1dbdab2d77e255e60e562e31629c1324d4655644
afonso cavichioli <108934804+jkprivxx@users.noreply.github.com>
2024-06-14 08:54:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".abaconteudo");
-
+const contador = document.querySelectorAll(".contador");
+contador[0].textContent="ola"
+const tempoObjetivo1 = new Date(2024,10,21,19,15);
+const tempoObjetivo2 = new Date(2025,10,11,18,05);
+const tempoObjetivo3 = new Date(2024,03,25,10.10);
+const tempoObjetivo4 = new Date(2025,05,22,00,00)
 
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
@@ -15,4 +20,6 @@ for (let i = 0; i < botoes.length; i++){
 
     }
 }
+
+
 
