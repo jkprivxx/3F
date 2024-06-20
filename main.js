1bd3301564cd26561a87c1db18650ab3beaa4e83
afonso cavichioli <108934804+jkprivxx@users.noreply.github.com>
2024-06-20 09:40:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,14 @@ const contador = document.querySelectorAll(".contador");
 contador[0].textContent="ola"
 const tempoObjetivo1 = new Date(2024,10,21,19,15);
 const tempoObjetivo2 = new Date(2025,10,11,18,05);
-const tempoObjetivo3 = new Date(2024,03,25,10.10);
-const tempoObjetivo4 = new Date(2025,05,22,00,00)
+const tempoObjetivo3 = new Date(2024,10,25,10.10);
+const tempoObjetivo4 = new Date(2025,05,22,00,00);
+let agora = new Date()
+contador[0].textContent= tempoObjetivo1 - "agora"
+contador[1].textContent= tempoObjetivo2 - "agora"
+contador[2].textContent= tempoObjetivo3 - "agora"
+contador[3].textContent= tempoObjetivo4 - "agora"
+
 
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
