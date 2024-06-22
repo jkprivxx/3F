5c0cfa44f56ff628cdf56e21d2a75c7e3c2c910d
afonso cavichioli <108934804+jkprivxx@users.noreply.github.com>
2024-06-22 01:16:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".abaconteudo");
 const contador = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date (2025,03,15);
-const tempoObjetivo2 = new Date (2025,05,15);
-const tempoObjetivo3 = new Date (2025,08,15);
-const tempoObjetivo4 = new Date (2025,10,15);
+const tempoObjetivo1 = new Date (2025,10,25);
+const tempoObjetivo2 = new Date (2025,11,25);
+const tempoObjetivo3 = new Date (2025,11,25);
+const tempoObjetivo4 = new Date (2025,10,25);
 let agora = new Date();
-let segundos
+let segundos 
 let minutos
 let horas
-let dias
-segundos = (tempoObjetivo1-agora)/1000;
+let dias 
+segundos = (tempoObjetivo1 - agora)/1000;
 minutos = segundos/60;
 horas = minutos/60;
 dias = horas/24;
-contador[0].textContent = `faltam ${ dias}dias, ${horas }horas, ${minutos}minutos e ${segundos}segundos´
+
+contador[0].textContent = `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos.`
 contador[1].textContent = tempoObjetivo2 - agora
 contador[2].textContent = tempoObjetivo3 - agora
 contador[3].textContent = tempoObjetivo4 - agora
-
 
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
@@ -32,4 +32,4 @@ for (let i = 0; i < botoes.length; i++){
 
 
     }
-}    
+}
