aa72ed001e994d2f8b247b3a60b82151cfc037e6
afonso cavichioli <108934804+jkprivxx@users.noreply.github.com>
2024-06-22 01:20:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".abaconteudo");
 const contador = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date (2025,02,25);
-const tempoObjetivo2 = new Date (2025,05,25);
-const tempoObjetivo3 = new Date (2025,08,25);
+const tempoObjetivo1 = new Date (2025,11,25);
+const tempoObjetivo2 = new Date (2025,10,25);
+const tempoObjetivo3 = new Date (2025,11,25);
 const tempoObjetivo4 = new Date (2025,10,25);
 let agora = new Date();
 let segundos 
