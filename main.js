40df195184f239b1a65edb84b723761a7031aea8
afonso cavichioli <108934804+jkprivxx@users.noreply.github.com>
2024-07-04 09:46:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -38,11 +38,24 @@ segundos =  Math.floor(segundos);
 minutos =  Math.floor(minutos);
 horas =  Math.floor(horas);
 dias =  Math.floor(dias);
+
 segundos = segundos%60;
 minutos = minutos%60;
 horas = horas%24;
+return[dias,horas,minutos,segundos]
 }
-
+let contadorPrimeiro = calculaTempo(tempoObjetivo1)
+contador[0].textContent = `faltam ${contadorPriemiro[0]} dias, ${contadorPriemro[1]} horas, ${contadorPrimeiro[2]} minutos ${contadorPriemiro[3]}`
+console.log(contadorPrimeiro)
+let contadorSegundo = calculaTempo(tempoObjetivo2)
+contador[1].textContent = `faltam ${contadorSegundo[0]} dias, ${contadorSegundo[1]} horas, ${contadorSegundo[2]} minutos ${contadorSegundo[3]}`
+console.log(contadorSegundo)
+let contadorTerceiro = calculaTempo(tempoObjetivo2)
+contador[2].textContent = `faltam ${contadorTerceiro[0]} dias, ${contadorTerceiro[1]} horas, ${contadorTerceiro[2]} minutos ${contadorTerceiro[3]}`
+console.log(contadorTerceiro)
+let contadorSegundo = calculaTempo(tempoObjetivo2)
+contador[3].textContent = `faltam ${contadorQuarto[0]} dias, ${contadorQuarto[1]} horas, ${contadorQuarto[2]} minutos ${contadorQuarto[3]}`
+console.log(contadorQuarto)
 contador[0].textContent = `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos.`
 contador[1].textContent = tempoObjetivo2 - agora
 contador[2].textContent = tempoObjetivo3 - agora
